1a506cf7a5759f8258923380e95effdc1e0ba5d7
WasperDev <contact@wasper.app>
2024-06-05 11:45:37 -0400

    Update index.ts

--- packages/backend/server/src/index.ts
+++ packages/backend/server/src/index.ts
@@ -6,11 +6,11 @@ import { Logger } from '@nestjs/common';
 import { createApp } from './app';
 
 const app = await createApp();
-const listeningHost = AFFiNE.deploy ? '0.0.0.0' : 'localhost';
-await app.listen(AFFiNE.port, listeningHost);
+const listeningHost = Wasper.deploy ? '0.0.0.0' : 'localhost';
+await app.listen(Wasper.port, listeningHost);
 
 const logger = new Logger('App');
 
-logger.log(`AFFiNE Server is running in [${AFFiNE.type}] mode`);
-logger.log(`Listening on http://${listeningHost}:${AFFiNE.port}`);
-logger.log(`And the public server should be recognized as ${AFFiNE.baseUrl}`);
+logger.log(`Wasper Server is running in [${Wasper.type}] mode`);
+logger.log(`Listening on http://${listeningHost}:${Wasper.port}`);
+logger.log(`And the public server should be recognized as ${Wasper.baseUrl}`);
