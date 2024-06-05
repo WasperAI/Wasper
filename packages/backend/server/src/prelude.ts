79617b5529216d03ed0f06412600e6c50f4ca7ff
WasperDev <contact@wasper.app>
2024-06-05 11:46:25 -0400

    Update prelude.ts

--- packages/backend/server/src/prelude.ts
+++ packages/backend/server/src/prelude.ts
@@ -9,7 +9,7 @@ import { omit } from 'lodash-es';
 
 import {
   applyEnvToConfig,
-  getDefaultAFFiNEConfig,
+  getDefaultWasperConfig,
 } from './fundamentals/config';
 
 const configDir = join(fileURLToPath(import.meta.url), '../config');
@@ -25,41 +25,41 @@ async function loadRemote(remoteDir: string, file: string) {
 }
 
 async function load() {
-  const AFFiNE_CONFIG_PATH = process.env.AFFINE_CONFIG_PATH ?? configDir;
-  // Initializing AFFiNE config
+  const Wasper_CONFIG_PATH = process.env.Wasper_CONFIG_PATH ?? configDir;
+  // Initializing Wasper config
   //
   // 1. load dotenv file to `process.env`
   // load `.env` under pwd
   config();
   // load `.env` under user config folder
   config({
-    path: join(AFFiNE_CONFIG_PATH, '.env'),
+    path: join(Wasper_CONFIG_PATH, '.env'),
   });
 
-  // 2. generate AFFiNE default config and assign to `globalThis.AFFiNE`
-  globalThis.AFFiNE = getDefaultAFFiNEConfig();
+  // 2. generate Wasper default config and assign to `globalThis.Wasper`
+  globalThis.Wasper = getDefaultWasperConfig();
 
   // TODO(@forehalo):
-  //   Modules may contribute to ENV_MAP, figure out a good way to involve them instead of hardcoding in `./config/affine.env`
-  // 3. load env => config map to `globalThis.AFFiNE.ENV_MAP
-  await loadRemote(AFFiNE_CONFIG_PATH, 'affine.env.js');
+  //   Modules may contribute to ENV_MAP, figure out a good way to involve them instead of hardcoding in `./config/Wasper.env`
+  // 3. load env => config map to `globalThis.Wasper.ENV_MAP
+  await loadRemote(Wasper_CONFIG_PATH, 'Wasper.env.js');
 
-  // 4. load `config/affine` to patch custom configs
-  await loadRemote(AFFiNE_CONFIG_PATH, 'affine.js');
+  // 4. load `config/Wasper` to patch custom configs
+  await loadRemote(Wasper_CONFIG_PATH, 'Wasper.js');
 
-  // 5. load `config/affine.self` to patch custom configs
-  // This is the file only take effect in [AFFiNE Cloud]
-  if (!AFFiNE.isSelfhosted) {
-    await loadRemote(AFFiNE_CONFIG_PATH, 'affine.self.js');
+  // 5. load `config/Wasper.self` to patch custom configs
+  // This is the file only take effect in [Wasper Cloud]
+  if (!Wasper.isSelfhosted) {
+    await loadRemote(Wasper_CONFIG_PATH, 'Wasper.self.js');
   }
 
-  // 6. apply `process.env` map overriding to `globalThis.AFFiNE`
-  applyEnvToConfig(globalThis.AFFiNE);
+  // 6. apply `process.env` map overriding to `globalThis.Wasper`
+  applyEnvToConfig(globalThis.Wasper);
 
-  if (AFFiNE.node.dev) {
+  if (Wasper.node.dev) {
     console.log(
-      'AFFiNE Config:',
-      JSON.stringify(omit(globalThis.AFFiNE, 'ENV_MAP'), null, 2)
+      'Wasper Config:',
+      JSON.stringify(omit(globalThis.Wasper, 'ENV_MAP'), null, 2)
     );
   }
 }
