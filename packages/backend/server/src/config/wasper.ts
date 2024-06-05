0b2383a3d11e697e50127e281df40e56ee85ac43
WasperDev <contact@wasper.app>
2024-06-05 11:43:42 -0400

    Update wasper.ts

--- packages/backend/server/src/config/wasper.ts
+++ packages/backend/server/src/config/wasper.ts
@@ -1,9 +1,9 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 //
 // ###############################################################
-// ##                AFFiNE Configuration System                ##
+// ##                Wasper Configuration System                ##
 // ###############################################################
-// Here is the file of all AFFiNE configurations that will affect runtime behavior.
+// Here is the file of all Wasper configurations that will affect runtime behavior.
 // Override any configuration here and it will be merged when starting the server.
 // Any changes in this file won't take effect before server restarted.
 //
@@ -11,8 +11,8 @@
 // > Configurations merge order
 //   1. load environment variables (`.env` if provided, and from system)
 //   2. load `src/fundamentals/config/default.ts` for all default settings
-//   3. apply `./affine.ts` patches (this file)
-//   4. apply `./affine.env.ts` patches
+//   3. apply `./Wasper.ts` patches (this file)
+//   4. apply `./Wasper.env.ts` patches
 //
 //
 // ###############################################################
@@ -20,28 +20,28 @@
 // ###############################################################
 //
 // /* The unique identity of the server */
-// AFFiNE.serverId = 'some-randome-uuid';
+// Wasper.serverId = 'some-randome-uuid';
 //
-// /* The name of AFFiNE Server, may show on the UI */
-// AFFiNE.serverName = 'Your Cool AFFiNE Selfhosted Cloud';
+// /* The name of Wasper Server, may show on the UI */
+// Wasper.serverName = 'Your Cool Wasper Selfhosted Cloud';
 //
 // /* Whether the server is deployed behind a HTTPS proxied environment */
-AFFiNE.https = false;
+Wasper.https = false;
 // /* Domain of your server that your server will be available at */
-AFFiNE.host = 'localhost';
+Wasper.host = 'localhost';
 // /* The local port of your server that will listen on */
-AFFiNE.port = 3010;
+Wasper.port = 3010;
 // /* The sub path of your server */
-// /* For example, if you set `AFFiNE.path = '/affine'`, then the server will be available at `${domain}/affine` */
-// AFFiNE.path = '/affine';
+// /* For example, if you set `Wasper.path = '/Wasper'`, then the server will be available at `${domain}/Wasper` */
+// Wasper.path = '/Wasper';
 //
 //
 // ###############################################################
 // ##                       Database settings                   ##
 // ###############################################################
 //
-// /* The URL of the database where most of AFFiNE server data will be stored in */
-// AFFiNE.db.url = 'postgres://user:passsword@localhost:5432/affine';
+// /* The URL of the database where most of Wasper server data will be stored in */
+// Wasper.db.url = 'postgres://user:passsword@localhost:5432/Wasper';
 //
 //
 // ###############################################################
@@ -50,26 +50,26 @@ AFFiNE.port = 3010;
 //
 // /* Whether enable metrics and tracing while running the server */
 // /* The metrics will be available at `http://localhost:9464/metrics` with [Prometheus] format exported */
-// AFFiNE.metrics.enabled = true;
+// Wasper.metrics.enabled = true;
 //
 // /* Authentication Settings */
 // /* Whether allow anyone signup */
-// AFFiNE.auth.allowSignup = true;
+// Wasper.auth.allowSignup = true;
 //
 // /* User Signup password limitation */
-// AFFiNE.auth.password = {
+// Wasper.auth.password = {
 //   minLength: 8,
 //   maxLength: 32,
 // };
 //
 // /* How long the login session would last by default */
-// AFFiNE.auth.session = {
+// Wasper.auth.session = {
 //   ttl: 15 * 24 * 60 * 60, // 15 days
 // };
 //
 // /* GraphQL configurations that control the behavior of the Apollo Server behind */
 // /* @see https://www.apollographql.com/docs/apollo-server/api/apollo-server */
-// AFFiNE.graphql = {
+// Wasper.graphql = {
 //   /* Path to mount GraphQL API */
 //   path: '/graphql',
 //   buildSchemaOptions: {
@@ -83,13 +83,13 @@ AFFiNE.port = 3010;
 //
 // /* Doc Store & Collaberation */
 // /* How long the buffer time of creating a new history snapshot when doc get updated */
-// AFFiNE.doc.history.interval = 1000 * 60 * 10; // 10 minutes
+// Wasper.doc.history.interval = 1000 * 60 * 10; // 10 minutes
 //
 // /* Use `y-octo` to merge updates at the same time when merging using Yjs */
-// AFFiNE.doc.manager.experimentalMergeWithYOcto = true;
+// Wasper.doc.manager.experimentalMergeWithYOcto = true;
 //
 // /* How often the manager will start a new turn of merging pending updates into doc snapshot */
-// AFFiNE.doc.manager.updatePollInterval = 1000 * 3;
+// Wasper.doc.manager.updatePollInterval = 1000 * 3;
 //
 //
 // ###############################################################
@@ -98,21 +98,21 @@ AFFiNE.port = 3010;
 //
 // /* Redis Plugin */
 // /* Provide caching and session storing backed by Redis. */
-// /* Useful when you deploy AFFiNE server in a cluster. */
-// AFFiNE.plugins.use('redis', {
+// /* Useful when you deploy Wasper server in a cluster. */
+// Wasper.plugins.use('redis', {
 //   /* override options */
 // });
 //
 //
 // /* Payment Plugin */
-// AFFiNE.plugins.use('payment', {
+// Wasper.plugins.use('payment', {
 //   stripe: { keys: {}, apiVersion: '2023-10-16' },
 // });
 //
 //
 // /* Cloudflare R2 Plugin */
 // /* Enable if you choose to store workspace blobs or user avatars in Cloudflare R2 Storage Service */
-// AFFiNE.plugins.use('cloudflare-r2', {
+// Wasper.plugins.use('cloudflare-r2', {
 //   accountId: '',
 //   credentials: {
 //     accessKeyId: '',
@@ -122,17 +122,17 @@ AFFiNE.port = 3010;
 //
 // /* AWS S3 Plugin */
 // /* Enable if you choose to store workspace blobs or user avatars in AWS S3 Storage Service */
-// AFFiNE.plugins.use('aws-s3', {
+// Wasper.plugins.use('aws-s3', {
 //  credentials: {
 //    accessKeyId: '',
 //    secretAccessKey: '',
 // })
 // /* Update the provider of storages */
-// AFFiNE.storage.storages.blob.provider = 'r2';
-// AFFiNE.storage.storages.avatar.provider = 'r2';
+// Wasper.storage.storages.blob.provider = 'r2';
+// Wasper.storage.storages.avatar.provider = 'r2';
 //
 // /* OAuth Plugin */
-// AFFiNE.plugins.use('oauth', {
+// Wasper.plugins.use('oauth', {
 //   providers: {
 //     github: {
 //       clientId: '',
